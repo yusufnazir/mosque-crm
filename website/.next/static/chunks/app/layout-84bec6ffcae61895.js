999ee5220635e6f0796c3bef6d8da1c934a2be06
Yusuf nazir <yusuf.nazir@gmail.com>
2026-04-20 11:23:13 -0300

    chore: remove Next.js build artifacts and cache files
    - Deleted various build and cache files from the .next directory to clean up the project
    - This includes manifests, build IDs, and other temporary files that are no longer needed

--- website/.next/static/chunks/app/layout-84bec6ffcae61895.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1290:()=>{},9056:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,1290,23))}},e=>{e.O(0,[741,441,255,358],()=>e(e.s=9056)),_N_E=e.O()}]);
